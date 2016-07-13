ef639a9ebeaaf74492679e6970386ef2264daf87
Jennifer Shannon <jen.shannon@gmail.com>
2016-07-13 11:04:21 -0400

    Add js_func

--- w2d2/if_else_ex/script.js
+++ w2d2/if_else_ex/script.js
@@ -1,10 +1,10 @@
 // if else
 
-if (false) {
-	console.log("This is my if");
-} else {
-	console.log("This is my else");
-}
+// if (false) {
+// 	console.log("This is my if");
+// } else {
+// 	console.log("This is my else");
+// }
 
 // # Exercises
 
@@ -13,26 +13,26 @@ if (false) {
 //it is, alert the user that their variable is less than 10. If it is not,
 // let the user know what the variable was and that it was greater than 10.
 
-var myVar = 12;
+// var myVar = 12;
 
-if (myVar < 10) {
-	alert("Your variable is less than 10");
-} else {
-	alert("Your variable " +myVar+ " is greater than 10");
-}
-
-
-// password example
-
-var password = "1234";
+// if (myVar < 10) {
+// 	alert("Your variable is less than 10");
+// } else {
+// 	alert("Your variable " +myVar+ " is greater than 10");
+// }
 
 
+// // password example
 
-if (password === "1235"){
-	alert("welcome to your social media account");
-} else {
-	alert("back off creep");
-}
+// var password = "1234";
+
+
+
+// if (password === "1235"){
+// 	alert("welcome to your social media account");
+// } else {
+// 	alert("back off creep");
+// }
 
 
 // using prompt
@@ -47,14 +47,14 @@ if (myVar < 10) {
 
 // password example with prompt
 
-var enteredPassword = prompt("Please enter your password");
-var storedPassword = "1234";
+// var enteredPassword = prompt("Please enter your password");
+// var storedPassword = "1234";
 
 
-if (enteredPassword === storedPassword){
-	alert("You have entered the right password. Welcome to your social media account.");
-} else {
-	alert("You have entered the wrong password.");
-}
+// if (enteredPassword === storedPassword){
+// 	alert("You have entered the right password. Welcome to your social media account.");
+// } else {
+// 	alert("You have entered the wrong password.");
+// }
 
 
---
+++ w2d3/js_func/script.js
@@ -0,0 +1,36 @@
+// function myFunc(){
+// 	console.log("Hi!");
+// 	console.log("How are you?");
+// 	alert("This is to annoy you?");
+// }
+
+// myFunc();
+
+// function someName(numberOne, otherNumber) {
+// 	console.log(numberOne + 10 + otherNumber);
+// }
+
+// someName(3,5);
+
+// # Exercises
+
+// - Declare a function that takes any string as an argument and console logs to the 
+//user the text they've entered, try invoking it after it has been declared
+
+
+// var userText = prompt("Enter some text:");
+
+// function yourString(anyString){
+	//console logs to the 
+	//user the text they've entered
+// 	console.log(anyString);
+// }
+
+// yourString(userText);
+
+// - Declare a function that takes no arguments but prints something to the console, try invoking it after it has been declared
+
+
+// - No Whammies!!! Declare a function that receives an input of door 1, 2 or 3. Depending upon which virtual "door" was entered, the function will tell the user they've received 
+//a different "prize" in an alert. Try running it after it has been declared a few 
+//times with each door option.
