c88b87022b31176c872529aa7b0bc2c0ee392739
Jennifer Shannon <jen.shannon@gmail.com>
2016-07-13 15:22:05 -0400

    Add jquery demo

--- w2d3/jquery_test/script.js
+++ w2d3/jquery_test/script.js
@@ -1,7 +1,33 @@
+// $(document).ready(function(){
+// // jquery magic happens here
+// 	$('img').click(function(){
+// 		$('p').slideUp(4000);
+// 		$('ul').fadeOut(6000);
+// 	});
+// });
+
+// var someOtherFunction = function(){
+// 	alert('hello world');
+// };
+
+// function someRunner(anyFunction){
+// 	console.log(2+2);
+// 	anyFunction();
+// }
+
+// someRunner(someOtherFunction);
+
 $(document).ready(function(){
 // jquery magic happens here
 	$('img').click(function(){
 		$('p').slideUp(4000);
 		$('ul').fadeOut(6000);
+		$('body').css("background-color","red");
+	});
+
+	
+
+	$(".menu").show(function(){
+		$(".menu").animate({ opacity: 0.5});
 	});
 });
