23adcb709345fa102e8930bf6433af92e4f7a387
Jennifer Shannon <jen.shannon@gmail.com>
2016-07-15 14:20:42 -0400

    Add logic and array to js clock

--- w2d5/js_clock/script.js
+++ w2d5/js_clock/script.js
@@ -6,21 +6,47 @@
 // console.log("This is minutes: "+ minutes);
 
 // document.getElementById("clock").innerHTML = myTime;
+var myArray = ["Bob", "Fred", "Sarah"];
+
+
+
+
+
+
 
 setInterval(function(){
 	var myTime = new Date();
-	var minutes = myTime.getMinutes();
-	if (minutes%2 === 0){
-		console.log("minutes is even");
+	var cheese = "good";
+	var minute = myTime.getMinutes();
+	var hours = myTime.getHours();
+	var seconds = myTime.getSeconds();
+	console.log(myArray[Math.floor(Math.random()*myArray.length)]);
+	document.getElementById("hours").innerHTML= hours;
+	document.getElementById("minutes").innerHTML= minute;
+	if (seconds%5 === 0 && cheese==="good"){
+		document.getElementById("home").style.backgroundColor = randomColor();
 	} else {
-		console.log("minutes is odd");
+		console.log("seconds is not a multiple of 5");
 	}
 	// document.body.style.backgroundColor = "pink";
-	document.getElementById("clock").innerHTML = myTime;
+	if (hours > 12) {
+		document.getElementById("message").innerHTML = "Good Afternoon";
+	}
+	
 }, 1000);
 
 
 // var myNum = 27;
 
+function randomColor(){
+	return '#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
+} 
+
+
+
+
+
+
+// console.log(myArray[Math.floor(Math.random()*myArray.length)]);
 
 
