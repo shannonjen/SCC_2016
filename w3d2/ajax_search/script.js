b07b6fdb70adb85e0e4d36d5aeacf586aa73c09e
Jennifer Shannon <jen.shannon@gmail.com>
2016-07-19 14:43:34 -0400

    Add notes

--- w3d2/ajax_search/script.js
+++ w3d2/ajax_search/script.js
@@ -1,17 +1,23 @@
 $(document).ready(function(){
 
 	$("#movie-form").on("submit", function(e){
+		//prevents default behavior of form submit button
 		e.preventDefault();
+		//pulls out the value entered input box
 		var movie = $("#movie-input").val();
+		//makes a call to ombdapi with requested movie
 		$.ajax({
 			type: "GET",
 			url: "http://www.omdbapi.com/?",
 			data: {
 				t: movie
 			},
+			//returns a json response on success
 			success: function(response){
+
 				console.log(response.Poster);
-				$('#image-holder').html("<img src="+response.Poster+">");
+				//selects image element and changes src attribute to moie poster image
+				$('img').attr("src",response.Poster);
 			}
 		});
 
