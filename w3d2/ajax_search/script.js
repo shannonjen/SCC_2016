e7211f7604727d79cce20902ce66551571ca789d
Jennifer Shannon <jen.shannon@gmail.com>
2016-07-20 11:45:30 -0400

    add one-page and google map ex

--- w3d2/ajax_search/script.js
+++ w3d2/ajax_search/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
-	$("#movie-form").on("submit", function(e){
+	$("#movie-input").on("keyup", function(){
 		//prevents default behavior of form submit button
-		e.preventDefault();
+		// e.preventDefault();
 		//pulls out the value entered input box
 		var movie = $("#movie-input").val();
 		//makes a call to ombdapi with requested movie
@@ -15,15 +15,16 @@ $(document).ready(function(){
 			//returns a json response on success
 			success: function(response){
 
-				console.log(response.Poster);
+				console.log(response);
 				//selects image element and changes src attribute to moie poster image
 				$('img').attr("src",response.Poster);
+				$('#movie-title').text(response.Title);
+				$('#movie-rating').text(response.Rated);
+				$('#plot').text(response.Plot);
 			}
 		});
 
-	});
+	})
 	
-
-
 
 });
