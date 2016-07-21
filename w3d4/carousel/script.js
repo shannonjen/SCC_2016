e89ea3c2609ace4a8cee5d1a40fda2e54fbb66d3
Jennifer Shannon <jen.shannon@gmail.com>
2016-07-21 12:28:31 -0400

    Add funky carousel

--- w3d4/carousel/script.js
+++ w3d4/carousel/script.js
@@ -1,12 +1,22 @@
-
-
-setInterval(someFunc, 4000);
-
-
-
+//array of the divs with a class of color-div
+var divs = $('.color-div');
+//counter to keep track of the number 
+//of the array item we are on
+var counter = 0;
 
 function someFunc(){	
-	$("#div1").show();
 
-	// alert("Function fired!");	
+	$(divs[counter]).show();
+	console.log(divs[counter]);
+	if (counter<3){
+		$(divs[counter-1]).hide();
+		counter++;
+	} else{
+		$(divs[counter-1]).hide();
+		counter = 0;
+	}	
+
 }
+
+
+setInterval(someFunc, 1000);
